3fde450587a3b0c2b1aea42fe9a6d87f50b7f43f
Geralt <rockygeralt@gmail.com>
2018-09-29 22:25:17 -0500

    Inicio de proyecto Angular para la U

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { CommonComponent } from './common/common.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    CommonComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
