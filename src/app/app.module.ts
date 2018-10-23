f299dbb63424174b1a19fad6c614c779dc59c478
William Mahecha <wsmh99@hotmail.com>
2018-10-23 11:17:12 -0500

    Configuracion de las rutas, componentes de registro y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 
-import { CommonComponent } from './common/common.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './home/home.component';
+
+import {ReactiveFormsModule} from '@angular/forms';
+
+const routes:Routes = [
+  {path:'', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component:RegistroComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    CommonComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    RegistroComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    CarouselModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
